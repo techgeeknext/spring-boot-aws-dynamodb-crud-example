17cdff5c2e54fff42b324f3fc6e5aa0eece6b19c
TechGeekNeXt <techgeeknext@gmail.com>
2021-06-06 19:21:14 +0530

    https://www.techgeeknext.com/spring-boot/spring-boot-aws-dynamodb-crud-example

---
+++ src/main/java/com/techgeeknext/SpringBootDynamodbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techgeeknext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDynamodbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDynamodbExampleApplication.class, args);
+	}
+
+}
